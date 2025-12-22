d234378c5430b573c6e2a10954ebe3722d2763b2
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-23 00:22:51 +0100

    feat: EthernetFrame

--- internal/parser/ethernet.go
+++ internal/parser/ethernet.go
@@ -1,9 +1,18 @@
 package parser
 
-const (
-	EthernetHeaderSize = 14
+import "net"
 
-	EtherTypeIPv4 = 0x0800
-	EtherTypeARP  = 0x0806
-	EtherTypeIPv6 = 0x86DD
+const (
+	EthernetHeaderSize = 14 // in bytes
+
+	EtherTypeIPv4 = uint16(0x0800)
+	EtherTypeARP  = uint16(0x0806)
+	EtherTypeIPv6 = uint16(0x86DD)
 )
+
+type EthernetFrame struct {
+	DestMAC   net.HardwareAddr
+	SrcMAC    net.HardwareAddr
+	EtherType uint16
+	Payload   []byte
+}
