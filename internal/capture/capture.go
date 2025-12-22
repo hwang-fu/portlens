d8aa34de00e9271110fbd1571033aecd583d553f
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-22 23:54:57 +0100

    feat: htons()

--- internal/capture/capture.go
+++ internal/capture/capture.go
@@ -1,6 +1,18 @@
 package capture
 
+import "syscall"
+
 // Socket represents a raw packet capture socket.
 type Socket struct {
 	fd int
+}
+
+// NewSocket creates a new AF_PACKET socket for capturing raw packets.
+func NewSocket() (*Socket, error) {
+	fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, int(htons(syscall.ETH_P_ALL)))
+}
+
+// htons converts a short (uint16) from host to network byte order.
+func htons(i uint16) uint16 {
+	return (i<<8)&0xff00 | i>>8
 }
