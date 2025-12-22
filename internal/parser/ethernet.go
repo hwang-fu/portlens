87a5515d4c70e29764dfaf53aafac86abf7d4a7e
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-23 00:25:41 +0100

    feat: ParseEthernet()

--- internal/parser/ethernet.go
+++ internal/parser/ethernet.go
@@ -1,6 +1,10 @@
 package parser
 
-import "net"
+import (
+	"encoding/binary"
+	"fmt"
+	"net"
+)
 
 const (
 	EthernetHeaderSize = 14 // in bytes
@@ -10,9 +14,24 @@ const (
 	EtherTypeIPv6 = uint16(0x86DD)
 )
 
+// EthernetFrame represents a parsed Ethernet frame header.
 type EthernetFrame struct {
 	DestMAC   net.HardwareAddr
 	SrcMAC    net.HardwareAddr
 	EtherType uint16
 	Payload   []byte
+}
+
+// ParseEthernet parses raw bytes into an EthernetFrame.
+func ParseEthernet(data []byte) (*EthernetFrame, error) {
+	if len(data) < EthernetHeaderSize {
+		return nil, fmt.Errorf("packet too short: %d bytes", len(data))
+	}
+
+	return &EthernetFrame{
+		DestMAC:   net.HardwareAddr(data[0:6]),
+		SrcMAC:    net.HardwareAddr(data[6:12]),
+		EtherType: binary.BigEndian.Uint16(data[12:14]),
+		Payload:   data[EthernetHeaderSize:],
+	}, nil
 }
