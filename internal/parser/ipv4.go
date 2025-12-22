0762238f0a89e8873c99978e29c3a2134ba14e85
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-23 00:45:32 +0100

    feat: IPv4Packet

--- internal/parser/ipv4.go
+++ internal/parser/ipv4.go
@@ -1,8 +1,22 @@
 package parser
 
+import "net"
+
 const (
 	IPv4MinHeaderSize = 20
 
 	ProtocolTCP = 6
 	ProtocolUDP = 17
 )
+
+// IPv4Packet represents a parsed IPv4 header.
+type IPv4Packet struct {
+	Version  uint8
+	IHL      uint8 // Header length in 32-bit words
+	TotalLen uint16
+	TTL      uint8
+	Protocol uint8
+	SrcIP    net.IP
+	DstIP    net.IP
+	Payload  []byte
+}
