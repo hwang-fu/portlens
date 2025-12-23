6e997043ec461796d306aa870a3135e05601dd0f
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-23 14:35:33 +0100

    feat: add Socket struct

--- internal/netlink/sockdiag.go
+++ internal/netlink/sockdiag.go
@@ -2,3 +2,8 @@ package netlink
 
 // NETLINK_SOCK_DIAG is the Netlink protocol for socket diagnostics.
 const NETLINK_SOCK_DIAG = 4
+
+// Socket represents a Netlink socket for querying socket information.
+type Socket struct {
+	fd int
+}
