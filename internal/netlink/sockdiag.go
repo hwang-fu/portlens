1a7a549553ca21ebb6474e22db7a4aaff335502e
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-23 14:35:54 +0100

    feat: add Socket.Close()

--- internal/netlink/sockdiag.go
+++ internal/netlink/sockdiag.go
@@ -1,9 +1,16 @@
 package netlink
 
+import "syscall"
+
 // NETLINK_SOCK_DIAG is the Netlink protocol for socket diagnostics.
 const NETLINK_SOCK_DIAG = 4
 
 // Socket represents a Netlink socket for querying socket information.
 type Socket struct {
 	fd int
+}
+
+// Close closes the Netlink socket.
+func (s *Socket) Close() error {
+	return syscall.Close(s.fd)
 }
