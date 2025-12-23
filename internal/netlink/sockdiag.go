5725945db52965f5365947491911d28c87a3571b
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-23 14:37:33 +0100

    feat: add NewSocket()

--- internal/netlink/sockdiag.go
+++ internal/netlink/sockdiag.go
@@ -1,6 +1,9 @@
 package netlink
 
-import "syscall"
+import (
+	"fmt"
+	"syscall"
+)
 
 // NETLINK_SOCK_DIAG is the Netlink protocol for socket diagnostics.
 const NETLINK_SOCK_DIAG = 4
@@ -8,6 +11,18 @@ const NETLINK_SOCK_DIAG = 4
 // Socket represents a Netlink socket for querying socket information.
 type Socket struct {
 	fd int
+}
+
+func NewSocket() (*Socket, error) {
+	fd, err := syscall.Socket(
+		syscall.AF_NETLINK,
+		syscall.SOCK_DGRAM,
+		NETLINK_SOCK_DIAG,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("create netlink socket: %w", err)
+	}
+	return &Socket{fd: fd}, nil
 }
 
 // Close closes the Netlink socket.
