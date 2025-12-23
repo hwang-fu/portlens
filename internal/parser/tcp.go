1795b35d3d46f9bd73da58c56c046b4c02008fe1
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-23 12:51:30 +0100

    feat: add tcp flag bit masks

--- internal/parser/tcp.go
+++ internal/parser/tcp.go
@@ -4,3 +4,13 @@ const (
 	TCPMinHeaderSize = 20 // in bytes
 	TCPMaxHeaderSize = 60 // in bytes (with options)
 )
+
+// TCP flag bitmasks (byte 13 of TCP header)
+const (
+	TCPFlagFIN = 0x01 // Finish - end of data
+	TCPFlagSYN = 0x02 // Synchronize - initiate connection
+	TCPFlagRST = 0x04 // Reset - abort connection
+	TCPFlagPSH = 0x08 // Push - send data immediately
+	TCPFlagACK = 0x10 // Acknowledgment
+	TCPFlagURG = 0x20 // Urgent (rarely used)
+)
