c07497866fcd10c01ee0dbca7ae0432cd35f310a
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-23 14:42:49 +0100

    feat: add readProcessName()

--- internal/procfs/process.go
+++ internal/procfs/process.go
@@ -1,6 +1,22 @@
 package procfs
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// ProcessInfo contains information about a process.
 type ProcessInfo struct {
 	PID  int
 	Name string
+}
+
+// readProcessName reads the process name from /proc/[pid]/comm.
+func readProcessName(pid int) string {
+	data, err := os.ReadFile(fmt.Sprintf("/proc/%d/comm", pid))
+	if err != nil {
+		return ""
+	}
+	return strings.TrimSpace(string(data))
 }
