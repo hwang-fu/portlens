79fb2c92503c88b1b7b261baa77c1141225164a3
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-23 12:40:27 +0100

    docs: UDPDiagram

--- internal/parser/udp.go
+++ internal/parser/udp.go
@@ -4,6 +4,8 @@ const (
 	UDPHeaderSize = 8
 )
 
+// UDPDatagram represents a parsed UDP header.
+// UDP is a connectionless, stateless protocol - each datagram is independent.
 type UDPDatagram struct {
 	SrcPort uint16
 	DstPort uint16
